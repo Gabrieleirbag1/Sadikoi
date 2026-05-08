612717191efaf58fe400905a5f4773c65698e4ff
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2026-05-08 14:06:05 +0200

    feat: add GroupComponent with template, styles, and tests

---
+++ src/app/components/group/group.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-group',
+  imports: [],
+  templateUrl: './group.component.html',
+  styleUrl: './group.component.css',
+})
+export class GroupComponent {}
