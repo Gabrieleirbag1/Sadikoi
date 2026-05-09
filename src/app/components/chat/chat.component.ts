f07d2f750d8bdb0de8dc9d771a3bd696aed3c535
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2026-05-09 19:05:59 +0200

    feat: implement chat component with message loading and sending functionality

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,9 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, signal } from '@angular/core';
+import { ChatService } from '../../services/chat/chat.service';
+import { AuthService } from '../../services/auth/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-chat',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css',
 })
-export class ChatComponent {}
+export class ChatComponent {
+
+  private readonly chatService = inject(ChatService);
+  private readonly authService = inject(AuthService);
+  private user: User | null = null;
+  protected messages = signal<Message[]>([]);
+
+  @Input() groupId!: number;
+
+  async ngOnInit(): Promise<void> {
+    const response = await this.authService.getLocalUser();
+    if (response) {
+      this.user = response;
+      this.loadMessages(this.groupId);
+    } else {
+      console.error('User not authenticated');
+    }
+  }
+
+  protected async loadMessages(groupId: number): Promise<void> {
+    try {
+      const response = await this.chatService.getMessages(groupId);
+      this.messages.set(response);
+      console.log('Loaded messages:', this.messages());
+    } catch (error) {
+      console.error('Error loading messages:', error);
+    }
+  }
+
+  protected async sendMessage(content: string): Promise<void> {
+    try {
+      if (!this.user) throw new Error('User not authenticated');
+      const newMessage = await this.chatService.sendMessage(this.groupId, content, this.user.id);
+      if (newMessage) this.messages.update(messages => [...messages, newMessage]);
+      console.log('Sent message:', newMessage);
+    } catch (error) {
+      console.error('Error sending message:', error);
+    }
+  }
+}
