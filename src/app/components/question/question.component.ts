382122aac4cc00809e02738f8c0caf243979aad8
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2026-05-11 16:38:43 +0200

    feat: implement QuestionComponent with question fetching logic and template integration

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, OnInit, signal } from '@angular/core';
+import { QuestionService } from '../../services/question/question.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-question',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './question.component.html',
   styleUrl: './question.component.css',
 })
-export class QuestionComponent {}
+export class QuestionComponent implements OnInit{
+
+  private readonly questionService = inject(QuestionService);
+
+  @Input() groupId!: number;
+
+  protected question = signal<Question | null>(null);
+
+  async ngOnInit(): Promise<void> {
+    await this.fetchQuestion();
+  }
+
+  protected async fetchQuestion(): Promise<void> {
+    try {
+      const question = await this.questionService.getQuestion(this.groupId);
+      this.question.set(question);
+      console.log('Fetched question:', this.question());
+    } catch (error) {
+      console.error('Error fetching question:', error);
+    }
+  }
+
+}
