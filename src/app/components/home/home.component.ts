11188c8ed3ee56a27f4b38383f9148637d21d29e
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2026-06-30 13:07:12 -0400

    feat: create suggestion + feedback component and implemented them in home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { GroupsComponent } from "../groups/groups.component";
 import { AccountComponent } from "../account/account.component";
 import { JoinGroupComponent } from '../join-group/join-group.component';
+import { FeedbackComponent } from "../feedback/feedback.component";
 
 @Component({
   selector: 'app-home',
-  imports: [GroupsComponent, JoinGroupComponent, AccountComponent],
+  imports: [GroupsComponent, JoinGroupComponent, AccountComponent, FeedbackComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   standalone: true
