c9c8adced1617fcc594280eb8e50712bfcf5c03b
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2026-06-30 12:47:07 -0400

    feat: bug report component

---
+++ src/app/components/feedback/bug-report/bug-report.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bug-report',
+  imports: [],
+  templateUrl: './bug-report.component.html',
+  styleUrl: './bug-report.component.css',
+})
+export class BugReportComponent {}
