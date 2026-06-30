276d34ac980c897c8e7f531f2f54f0ccc199cdfe
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2026-06-30 14:19:12 -0400

    build: add ngx-translate for internationalization support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,26 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideTranslateService} from '@ngx-translate/core';
+import { provideTranslateHttpLoader} from '@ngx-translate/http-loader';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { authInterceptor } from './interceptors/auth.interceptor'; // Ajustez le chemin d'accès
-
+import { authInterceptor } from './interceptors/auth.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(
       withFetch(),
-      withInterceptors([authInterceptor]) // <-- Intercepteur enregistré ici de manière moderne
+      withInterceptors([authInterceptor]) 
     ),
     provideRouter(routes),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: '/assets/i18n/',
+        suffix: '.json'
+      }),
+      fallbackLang: 'en',
+      lang: 'en'
+    })
   ]
 };
