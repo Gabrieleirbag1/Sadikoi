c949a957743ade1f4b822b578103bdd93b0c2108
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2026-07-02 15:11:42 -0400

    feat: implement footer component and update home layout with state management

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GroupsComponent } from "../groups/groups.component";
 import { AccountComponent } from "../account/account.component";
-import { JoinGroupComponent } from '../join-group/join-group.component';
 import { FeedbackComponent } from "../feedback/feedback.component";
+import { CommonModule } from '@angular/common';
 
+type HomeState = 'groups' | 'account';
 @Component({
   selector: 'app-home',
-  imports: [GroupsComponent, JoinGroupComponent, AccountComponent, FeedbackComponent],
+  imports: [GroupsComponent, AccountComponent, FeedbackComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   standalone: true
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  protected homeState = 'groups'
+
+  public ngOnInit(): void {
+    const homeState = sessionStorage.getItem('homeState') as HomeState || 'groups';
+    this.changeHomeState(homeState);
+  }
+
+  protected changeHomeState(newState: HomeState): void {
+    sessionStorage.setItem('homeState', newState);
+    this.homeState = newState;
+  }
+
+}
