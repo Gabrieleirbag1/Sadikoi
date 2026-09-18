28f405e27a18b5ae2da44c72e4de34ccee3c5add
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2026-09-18 16:29:46 +0200

    feat(enhance): profile image picker with clickable input and improved styles

--- src/app/components/profile-image-picker/profile-image-picker.component.ts
+++ src/app/components/profile-image-picker/profile-image-picker.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, model } from '@angular/core';
+import { Component, computed, ElementRef, inject, model, viewChild } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { environment } from '../../../environments/environment.development';
 
@@ -16,6 +16,7 @@ export class ProfileImagePickerComponent {
   public readonly fileSelected = model<File | null>(null);
   public readonly onlyDisplay = model<boolean>(false);
   public readonly imgClass = model<string>('');
+  private readonly fileInput = viewChild<ElementRef<HTMLInputElement>>('fileInput');
   
   protected readonly profilePictureUrl: string | null = environment.apiUrl + '/auth/profile-picture/';
   protected readonly previewUrl = model<string | null>(null);
@@ -38,6 +39,10 @@ export class ProfileImagePickerComponent {
   public clearPreview(): void {
     this.previewUrl.set(null);
     this.fileSelected.set(null);
+  }
+
+  protected openFilePicker(): void {
+    this.fileInput()?.nativeElement.click();
   }
 
   protected onFileChange(event: Event): void {
